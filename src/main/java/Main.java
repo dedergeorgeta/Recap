001ebe5e38d898ca04e6342aaeea58bbf4191ab3
Deder Georgeta <t.getty_18@yahoo.com>
2020-10-18 13:16:47 +0300

    add a new sout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World.");
         System.out.println("Hello Geta");
+        System.out.println("Switch to main branch again");
     }
 }
