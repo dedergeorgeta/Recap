8ebdc11cae9cabe0be671fee8cdeca2e03d32674
Deder Georgeta <t.getty_18@yahoo.com>
2020-10-18 13:08:57 +0300

    Add input to test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World.");
         System.out.println("Hello Geta");
+        System.out.println("Change the test branch");
     }
 }
