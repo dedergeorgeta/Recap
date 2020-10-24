e7822ac8d7682cae04cd0e9ea5974ceb91655cbb
Deder Georgeta <t.getty_18@yahoo.com>
2020-10-24 11:14:26 +0300

    commit after stash

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World.");
+        System.out.println("Hello Geta");
+        System.out.println("Switch to main branch again");
+        System.out.println("new line");
         System.out.println("Hello Geta.");
         System.out.println("Switch to main branch again.");
         System.out.println("Adaugam o linie noua in main.");
