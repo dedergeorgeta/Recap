fd696667a5a9686ec8319b73a37bb967361fd0c4
Deder Georgeta <t.getty_18@yahoo.com>
2020-10-24 09:47:20 +0300

    Add new line to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World.");
-        System.out.println("Hello Geta");
-        System.out.println("Switch to main branch again");
+        System.out.println("Hello Geta.");
+        System.out.println("Switch to main branch again.");
+        System.out.println("Adaugam o linie noua in main.");
+
     }
 }
