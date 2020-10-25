396035ddd0618c32d18ce98dcc389933e592254a
Deder Georgeta <t.getty_18@yahoo.com>
2020-10-25 13:56:50 +0200

    algorithm isPalindrome

---
+++ src/main/java/algorithms/ArrayUtils.java
@@ -0,0 +1,20 @@
+package algorithms;
+
+import java.lang.reflect.Array;
+
+public class ArrayUtils {
+    public static boolean isPalindrome(int [] n ) {
+        //Palindrome: int [n] = {1,2,5,5,2,1}
+        boolean isPalindrome = false;
+        for (int i = 0, j = n.length - 1; i <= j; i++, j--) {
+            if (n[i] != n[j]) {
+                isPalindrome = false;
+                break;
+            } else {
+                isPalindrome = true;
+            }
+        }
+        return isPalindrome;
+    }
+
+}
